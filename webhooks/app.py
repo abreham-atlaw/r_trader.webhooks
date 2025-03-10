9397f23453bf65e4184fae6a0422f6bbbb655699
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-03-10 11:02:41 +0300

    Error handling

--- webhooks/app.py
+++ webhooks/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+import time
+
 from kaggle_manager import run_notebook
 
 app = Flask(__name__)
@@ -7,8 +9,17 @@ app = Flask(__name__)
 
 @app.route("/notebooks/notebook-runner")
 def notebook_runner():
-	run_notebook("bemnetatlaw/notebookrunner", {})
-	return "", 200
+	from kaggle.rest import ApiException
+	try:
+		run_notebook("bemnetatlaw/notebookrunner", {})
+		return "", 200
+	except ApiException as ex:
+		if ex.status in [429, 404]:
+			print(f"Rate limited[{ex.status}]. Waiting 30 seconds...")
+			time.sleep(30)
+			return notebook_runner()
+		else:
+			raise ex
 
 
 if __name__ == "__main__":
